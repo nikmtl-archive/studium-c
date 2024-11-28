1fd62f81d22f748a8a76a3ff28d6002614e566bb
Nik <127861651+Edamame04@users.noreply.github.com>
2024-11-28 21:41:54 +0100

    601

---
+++ Sem1/Programieren in C/CLionProjects/Einführung/cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/601-address-list.c.obj"
+  "CMakeFiles/untitled.dir/601-address-list.c.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
