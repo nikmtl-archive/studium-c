90c741315e45372a4651684052c16624312e729f
Nik <127861651+NikMitzel@users.noreply.github.com>
2024-12-04 21:40:23 +0100

    706 -> todo 4

--- Sem1/Programieren in C/CLionProjects/Einführung/706-linked_list.c
+++ Sem1/Programieren in C/CLionProjects/Einführung/706-linked_list.c
@@ -12,14 +12,14 @@
    
    The code below already contains some important definitions and functions.
 
-   TO DO 1: Have a look at the program, get it to run and try to understand how it works.
+✓  TO DO 1: Have a look at the program, get it to run and try to understand how it works.
             - Print out pointer values to keep track of what the program does.
-			- At what position is a new item added to the list: beginning or end?
-   TO DO 2: Extend the main function such that more items are added to the list
-   TO DO 3: Complete the function print_items to print for each item in the list:
+			- At what position is a new item added to the list: beginning or end?           Answer: Beginning
+✓  TO DO 2: Extend the main function such that more items are added to the list
+✓  TO DO 3: Complete the function print_items to print for each item in the list:
 			- the name, the address in memory and the address of its successor
             In addition, calculate the sum of all data payloads.
-   TO DO 4: Write a function find_item(...) that searches the list for an item with a given guid.
+✓  TO DO 4: Write a function find_item(...) that searches the list for an item with a given guid.
    TO DO 5: Change the program such that the memory required for 'data' can be allocated flexibly:
             in struct: unsigned char *data;
 	        ptr->data=malloc(ptr->size*sizeof(unsigned char)); // Remember to check for NULL Pointer
@@ -35,86 +35,102 @@
 #include <stdlib.h>
 
 // Data structure of the individual items in the list.
-struct myListItemStruct { 
-    int guid;                 // a unique id to identify the list item
-    char name[128];           // a name to represent the item 
-	unsigned char data[200];  // arbitrary data 'payload'
-	int size;                 // size of data
-    struct myListItemStruct *next_item;   // pointer to next item
+struct myListItemStruct {
+    int guid; // a unique id to identify the list item
+    char name[128]; // a name to represent the item
+    unsigned char data[200]; // arbitrary data 'payload'
+    int size; // size of data
+    struct myListItemStruct *next_item; // pointer to next item
 };
 
 // Define an own type for shorter declarations
-typedef struct myListItemStruct itemType;    
+typedef struct myListItemStruct itemType;
 
 // create_item:
 // Creates and initializes a new item, or returns NULL, if it could not allocate memory.
-itemType *create_item(){
-   static int guid_ctr=0;    // static counter to create unique ids
-   itemType *ptr;
+itemType *create_item() {
+    static int guid_ctr = 0; // static counter to create unique ids
+    itemType *ptr;
 
-   ptr=malloc(sizeof(itemType));   // Allocate memory for the list item
-   if(ptr!=NULL) {
-	  ptr->guid=guid_ctr++;   // Generate a unique ID
-      printf("Enter name");
-      scanf("%s",ptr->name);  // Enter a name
-      // TODO ptr->data[..] = ....
-      // TODO ptr->size=...;
-   }
-   return ptr;    // Return pointer to the new item
+    ptr = malloc(sizeof(itemType)); // Allocate memory for the list item
+    if (ptr != NULL) {
+        ptr->guid = guid_ctr++; // Generate a unique ID
+        printf("Enter name:");
+        scanf("%s", ptr->name); // Enter a name
+        printf("Enter Data:");
+        scanf("%s", ptr->data);
+        int count = 0;
+        while (ptr->data[count] != '\0') count++;
+        ptr->size = count;
+    }
+    return ptr; // Return pointer to the new item
 }
 
 // Insert_item:
 // Inserts an existing item into the (potentially empty) list
-void insert_item(itemType *new_item, itemType **list_ptr){
-	new_item->next_item=*list_ptr;   // Append existing list to new item
-	*list_ptr=new_item;              // Set list_ptr to points to new item
+void insert_item(itemType *new_item, itemType **list_ptr) {
+    new_item->next_item = *list_ptr; // Append existing list to new item
+    *list_ptr = new_item; // Set list_ptr to points to new item
 }
 
 
 // print all items
-void print_items(itemType *list_start)
-{
-// TO DO.... 
-
+void print_items(itemType *list_start) {
+    itemType *current = list_start;
+    long sum = 0;
+    while (current != NULL) {
+        printf("GUID: %d; ", current->guid);
+        printf("NAME: %s; ", current->name);
+        printf("DATA: ");
+        for (int i = 0; i < current->size; ++i) printf("%c", current->data[i]);
+        printf("; ADDRESS: %p; ", current);
+        current->next_item == NULL ? printf("END\n") : printf("NEXT: %p\n", current->next_item);
+        sum += current->size;
+        current = current->next_item;
+    }
+    printf("Sum of all data sizes: %ld\n", sum);
 }
 
-// Finds an item with guid in the list 
-itemType *find_item(int guid, itemType *list_start)
-{
-
+// Finds an item with guid in the list (linear search)
+itemType *find_item(int guid, itemType *list_start) {
+    itemType *current = list_start;
+    while (current != NULL) {
+        if (current->guid == guid) return current;
+        current->next_item;
+    }
+    return NULL;
 }
 
 // Deletes an item and returns the successor of the deleted item
-// The chaining of the previous and next element has to be done 
+// The chaining of the previous and next element has to be done
 // by the caller.
-itemType *simple_delete_item(itemType *item)
-{
-
-
+itemType *simple_delete_item(itemType *item) {
 }
 
 // Complete delete item
 // Deletes item 'to_delete' from the 'list'
 // return 0, if the item does not exist in the list, otherwise 1
-int delete_item(itemType *to_delete, itemType **list_ptr)
-{
-	// TO DO: Check if list (i.e. *list_ptr) is not NULL
-	// TO DO: Check special case, if first list element is to be deleted
-	//        The list_ptr needs to be changed, to point to second list element
-	// TO DO: General case: move through list with a helper pointer
-	// TO DO: Don't forget to free the memory of the list element (TODO5: and its data payload)
+int delete_item(itemType *to_delete, itemType **list_ptr) {
+    // TO DO: Check if list (i.e. *list_ptr) is not NULL
+    // TO DO: Check special case, if first list element is to be deleted
+    //        The list_ptr needs to be changed, to point to second list element
+    // TO DO: General case: move through list with a helper pointer
+    // TO DO: Don't forget to free the memory of the list element (TODO5: and its data payload)
 }
 
 
-int main() 
-{
-    itemType *my_list=NULL;
+int main() {
+    itemType *my_list = NULL;
     itemType *current;
 
-    current=create_item();
-    insert_item(current,&my_list);
-	current=create_item();
-    insert_item(current,&my_list);
+    current = create_item();
+    insert_item(current, &my_list);
+    current = create_item();
+    insert_item(current, &my_list);
+    current = create_item();
+    insert_item(current, &my_list);
+    current = create_item();
+    insert_item(current, &my_list);
 
-	print_items(my_list);
+    print_items(my_list);
 }
